c106dc69174b647e54b549252ea26c12f2be5cfb
Mohit Srivastava <iammohitpro@gmail.com>
2020-11-09 20:15:54 +0530

    cors resolver16

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   next()
 })
 const server = http.createServer(app);
-const io = require('socket.io')(server, { origins: '*:*'});
+const io = require('socket.io')(server);
 
 
 app.use(router);
