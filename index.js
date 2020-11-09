ac20f9cad648394db09db31dce13e2523f8b0f09
Mohit Srivastava <iammohitpro@gmail.com>
2020-11-09 16:17:00 +0530

    cors resolver4

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const router = require('./router');
 
 const app = express();
 app.use(cors());
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
-})
+}) */
 const server = http.createServer(app);
 const io = socketio(server);
 
