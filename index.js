1c53213b672bd07380288ae3f3733b10f24be630
Mohit Srivastava <iammohitpro@gmail.com>
2020-11-09 20:35:54 +0530

    cors resolver18

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 const router = require('./router');
 
 const app = express();
-app.use(cors());
+/* app.use(cors()); */
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
