30edd9cdd1b6630d91eae6c29158b9a35c619d4a
Mohit Srivastava <iammohitpro@gmail.com>
2020-11-09 13:37:58 +0530

    cors resolver

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 app.use(router);
 
 io.on('connect', (socket) => {
