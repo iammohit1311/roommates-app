46cc9bf832e8d8694a900acb8772267f47c89c07
Mohit Srivastava <iammohitpro@gmail.com>
2020-11-09 20:58:36 +0530

    cors resolver22

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const router = require('./router');
 const app = express();
 app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://roommates-chat-app.herokuapp.com/")
+  res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   res.header("Access-Control-Allow-Credentials", "true")
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH")
